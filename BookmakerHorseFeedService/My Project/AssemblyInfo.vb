7defc18210dba4baeb30a5e9d4f9115e552e1cdd
paulowensmith68 <paulosmith@tiscali.co.uk>
2016-05-12 04:08:10 +0100

    Add project files.

---
+++ BookmakerHorseFeedService/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BookmakerHorseFeedService")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard")>
+<Assembly: AssemblyProduct("BookmakerHorseFeedService")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8aeacc2e-64e4-48f9-a008-723dd34e3d71")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
